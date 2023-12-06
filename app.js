fef5a7eb9d67f0062456a0d40c2a3e278bc64dd4
masahub <f.u.bar.base@gmail.com>
2023-12-07 06:54:38 +0900

    add cat api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var catRouter = require('./routes/cat');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/cat', catRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
